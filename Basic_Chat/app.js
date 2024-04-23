ebd54e211cfb1569791e6c0c226d2c379e488024
Khchimi Othmen <80161433+khchimi-Othmen@users.noreply.github.com>
2024-04-23 21:11:57 +0100

    Adding socket in Node js

--- Basic_Chat/app.js
+++ Basic_Chat/app.js
@@ -2,13 +2,33 @@
 const express = require('express');
 
 const app = express();
+
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT,()=>{
         console.log('Server is Stated on',PORT);
 });
 
 const io = require('socket.io')(server);
+ 
+const connectedUser = new Set();
 
-io.on('connection',(socket)=>{
-console.log("Connected Successufully",socket.id);
-});
+io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+    io.emit('connected-user', connectedUser.size)
+    connectedUser.add(socket.id)
+    io.emit('connected-user', connectedUser.size)
+
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+      connectedUser.delete(socket.id)
+      io.emit('connected-user', connectedUser.size)
+
+    });
+  
+    socket.on('message', (data) => {
+      console.log(data);
+      socket.broadcast.emit('message-recieve',data)
+    });
+  });
+  
