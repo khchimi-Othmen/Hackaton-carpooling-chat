3d8917b811af0cbed0e0c2fb6a2a470c5eeb4150
Khchimi Othmen <80161433+khchimi-Othmen@users.noreply.github.com>
2024-04-25 14:21:19 +0100

    fixed broadcast

--- Basic_Chat/app.js
+++ Basic_Chat/app.js
@@ -1,3 +1,4 @@
+
 // console.log('working22');
 const express = require('express');
 
@@ -5,30 +6,29 @@ const app = express();
 
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT,()=>{
-        console.log('Server is Stated on',PORT);
+        console.log('Server is Stated on',PORT);
 });
 
 const io = require('socket.io')(server);
- 
+ 
 const connectedUser = new Set();
 
 io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
-    io.emit('connected-user', connectedUser.size)
-    connectedUser.add(socket.id)
-    io.emit('connected-user', connectedUser.size)
+    console.log('Client connected:', socket.id);
+    io.emit('connected-user', connectedUser.size)
+    connectedUser.add(socket.id)
+    io.emit('connected-user', connectedUser.size)
 
 
-    socket.on('disconnect', () => {
-      console.log('Client disconnected:', socket.id);
-      connectedUser.delete(socket.id)
-      io.emit('connected-user', connectedUser.size)
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+      connectedUser.delete(socket.id)
+      io.emit('connected-user', connectedUser.size)
 
-    });
-  
-    socket.on('message', (data) => {
-      console.log(data);
-      socket.broadcast.emit('message-recieve',data)
-    });
-  });
-  
+    });
+  
+    socket.on('message', (data) => {
+      console.log(data);
+      socket.broadcast.emit('message-recieve',data)
+    });
+  });
